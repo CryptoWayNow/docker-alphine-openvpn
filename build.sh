0cf455fa278fa0b7552d4dac42877324920b974a
BlackPanda <46260610+B14ckP4nd4@users.noreply.github.com>
2021-06-16 22:52:51 -0700

    test args on builder script

--- build.sh
+++ build.sh
@@ -1,71 +1,39 @@
 #!/bin/bash
 
-# Process parameters
-params="$(getopt -o d:e:fshv \
-    -l diff:,exclude:,force,skip-existing,help,verbose \
-    --name "$cmdname" -- "$@")"
 
+# Process parameters
 if [ $? -ne 0 ]
 then
-    usage
+    echo ""
+    done < "$0"
 fi
 
-eval set -- "$params"
-unset params
-
-while true
-do
-    case $1 in
-        -d|--diff)
-            diff_exec=(${2-})
-            shift 2
-            ;;
-        -e|--exclude)
-            # Will override $default_excludes
-            excludes+=("${2-}")
-            shift 2
-            ;;
-        -f|--force)
-            action='R'
-            shift
-            ;;
-        -s|--skip-existing)
-            action='S'
-            shift
-            ;;
-        -h|--help)
-            usage
-            exit
-            ;;
-        -v|--verbose)
-            verbose='--verbose'
-            shift
-            ;;
-        --)
-            shift
-            if [ -z "${1:-}" ]
-            then
-                error "Missing targets." "$help_info" $EX_USAGE
-            fi
-            if [ -z "${2:-}" ]
-            then
-                error "Missing directory." "$help_info" $EX_USAGE
-            fi
-            targets=(${@:1:$(($#-1))})
-            source_dir="${@:$#}"
-            break
-            ;;
-        *)
-            usage
-            ;;
+_setArgs(){
+  while [ "${1:-}" != "" ]; do
+    case "$1" in
+      "-c" | "--config_file")
+        shift
+        configFile=$1
+        ;;
+      "-f" | "--forceUpdate")
+        forceUpdate=true
+        ;;
+      "-r" | "--forceRetry")
+        forceRetry=true
+        ;;
     esac
-done
-
+    shift
+  done
+}
 
 # Remove old image
 
+echo $configFile
+echo $forceUpdate
+echo $forceRetry
 
-if [[ "$(docker images -q myimage:mytag 2> /dev/null)" == "" ]]; then
-  # do something
 
-fi
+# if [[ "$(docker images -q myimage:mytag 2> /dev/null)" == "" ]]; then
+#   # do something
+
+# fi

ccb7208beadc7cda69266bb5d641524234774e92
BlackPanda <46260610+B14ckP4nd4@users.noreply.github.com>
2021-06-16 23:33:32 -0700

    set args and script for builder to create the container

--- build.sh
+++ build.sh
@@ -2,38 +2,51 @@
 
 
 # Process parameters
-if [ $? -ne 0 ]
-then
-    echo ""
-    done < "$0"
+PORT=443
+PROTOCOL='tcp'
+CONFIG_PATH="/home/ovpn"
+
+if [[ "$1" ]]; then
+    PORT=$1
 fi
 
-_setArgs(){
-  while [ "${1:-}" != "" ]; do
-    case "$1" in
-      "-c" | "--config_file")
-        shift
-        configFile=$1
-        ;;
-      "-f" | "--forceUpdate")
-        forceUpdate=true
-        ;;
-      "-r" | "--forceRetry")
-        forceRetry=true
-        ;;
-    esac
-    shift
-  done
-}
+if [[ "$2" ]]; then
+    PROTOCOL=$2
+fi
+
+if [[ "$3" ]]; then
+    CONFIG_PATH=$3
+fi
+
+
+echo "recreate config path"
+rm -rf $CONFIG_PATH/$PROTOCOL
+mkdir -p $CONFIG_PATH/$PROTOCOL
 
 # Remove old image
 
-echo $configFile
-echo $forceUpdate
-echo $forceRetry
+IMAGE_NAME="ovpn-$PROTOCOL"
+
+echo "Remove Container and IMAGES if exist"
+docker images -a | grep "$IMAGE_NAME" | awk '{print $3}' | xargs docker rmi -f
+docker ps -a | grep "$IMAGE_NAME" | awk '{print $1}' | xargs docker rm -f
 
 
-# if [[ "$(docker images -q myimage:mytag 2> /dev/null)" == "" ]]; then
-#   # do something
 
-# fi
+docker build \
+  --no-cache \
+  -t $IMAGE_NAME .
+
+
+docker create \
+  --cap-add=NET_ADMIN \
+  --name=ovpn-$PROTOCOL \
+  --network=host \
+  --privileged \
+  -e PROTO=tcp \
+  -e PORT=1194 \
+  -e INTERFACE='eth0' \
+  -p $PORT:443/$PROTOCOL \
+  -v $CONFIG_PATH:/config \
+  --restart always \
+  $IMAGE_NAME
